8b4856d3da6c072dddf9be66c0a1c0f3f9c8dd42
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-04-17 14:49:42 +0530

    Added uc2

--- src/test/java/JavaMood.java
+++ src/test/java/JavaMood.java
@@ -7,23 +7,23 @@ import JavaDay21.MoodAnalyzer;
 
 public class JavaMood {
 	    @Test
-	    public void sadMood() {
-	    	MoodAnalyzer sc = new MoodAnalyzer("Output is a sad ");
-	        String mood = sc.analyMood();
+	    public void testMood_MoodIsSad() {
+	    	MoodAnalyzer ma = new MoodAnalyzer("I am in sad mood");
+	        String mood = ma.analyMood();
 	        assertEquals(mood,"SAD");
 	    }
 	    @Test
-	    public void happyMood() {
-	    	MoodAnalyzer sc = new MoodAnalyzer("OutPut is a happy ");
-	        String mood = sc.analyMood();
-	        assertEquals(mood,"HAPPY");
+	    public void testMood_MoodIsHappy() {
+	    	MoodAnalyzer ma = new MoodAnalyzer("I am in any mood");
+	        String mood1 = ma.analyMood();
+	        assertEquals(mood1,"HAPPY");
 	    }
 
       @Test
       public void Null() {
-    	  MoodAnalyzer sc = new MoodAnalyzer(null);
-      String mood3 = sc.analyMood();
-    assertEquals(mood3,"HAPPY");
+    	  MoodAnalyzer ma = new MoodAnalyzer(null);
+      String mood2 = ma.analyMood();
+    assertEquals(mood2,"HAPPY");
 
 }
 }
